8ebdb5bd2819a8762b1f875e54d18648313c3d9d
tejeshvaish <55567070+tejeshvaish@users.noreply.github.com>
2020-04-25 11:36:00 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,13 +1,11 @@
 import requests
 for x in range(100):
-        url='https://docs.google.com/forms/d/e/1FAIpQLSfxsg59ggPdonbOLakPTwn_qQk-P0euw531kGt2pdDxSlnB9Q/viewform'
-        submission ={
-                
-	         'entry.1156409':'Tejesh Vaish'
-	           ,'entry.210055283':'190908',
-	           'entry.771155771':'Yes',
-	            'entry.446099277':'https://github.com/tejeshvaish/8-languages-in-8-weeks'
-              
-              
-        } 
-        requests.post(url, submission)
+         url='https://docs.google.com/forms/d/e/1FAIpQLSfxsg59ggPdonbOLakPTwn_qQk-P0euw531kGt2pdDxSlnB9Q/viewform'
+         submission={
+        'entry.1156409':'Tejesh Vaish'
+       ,'entry.210055283':'190908',
+       'entry.771155771':'Yes',
+       'entry.446099277':'https://github.com/tejeshvaish/8-languages-in-8-weeks'
+         
+      }
+         requests.post(url, submission)
