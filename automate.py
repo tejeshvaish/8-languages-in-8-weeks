154ce0d4beee215cceed4606cc4ada94dd98a427
tejeshvaish <55567070+tejeshvaish@users.noreply.github.com>
2020-04-25 11:28:47 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -6,7 +6,7 @@ for x in range(100):
 	         'entry.1156409':'Tejesh Vaish'
 	           ,'entry.210055283':'190908',
 	           'entry.771155771':'Yes',
-	            'entry.446099277':'https://docs.google.com/forms/d/e/1FAIpQLSfxsg59ggPdonbOLakPTwn_qQk-P0euw531kGt2pdDxSlnB9Q/viewform'
+	            'entry.446099277':'https://github.com/tejeshvaish/8-languages-in-8-weeks'
               
               
         } 
