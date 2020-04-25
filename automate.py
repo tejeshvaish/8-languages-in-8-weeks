10b68ea9c7bd38e7a6186c190939c340b0786ccd
tejeshvaish <55567070+tejeshvaish@users.noreply.github.com>
2020-04-25 11:28:07 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,16 +1,13 @@
-import urllib
-from urllib.request import Request 
-
-user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
-header={'User-Agent' : user_agent}
-url = "https://docs.google.com/forms/d/e/1FAIpQLSfxsg59ggPdonbOLakPTwn_qQk-P0euw531kGt2pdDxSlnB9Q/viewform"
-# values from your form. You will need to include any hidden variables if you want to..
-values= {
-'Name': 'Tejesh Vaish',
-'Roll Number':'190908',
-'Did you use a python script':'Yes',
-'Link to github repo/pastebin where I can see your script':'https://github.com/tejeshvaish/8-languages-in-8-weeks/tree/masterP0euw531kGt2pdDxSlnB9Q/viewform'
-}
-print(values)
-data = urllib.parse.urlencode(values)
-Request(url, data,header )
+import requests
+for x in range(100):
+        url='https://docs.google.com/forms/d/e/1FAIpQLSfxsg59ggPdonbOLakPTwn_qQk-P0euw531kGt2pdDxSlnB9Q/viewform'
+        submission ={
+                
+	         'entry.1156409':'Tejesh Vaish'
+	           ,'entry.210055283':'190908',
+	           'entry.771155771':'Yes',
+	            'entry.446099277':'https://docs.google.com/forms/d/e/1FAIpQLSfxsg59ggPdonbOLakPTwn_qQk-P0euw531kGt2pdDxSlnB9Q/viewform'
+              
+              
+        } 
+        requests.post(url, submission)
